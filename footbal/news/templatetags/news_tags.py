92a6ac702e63cb4971b119e6e94410f8118e24c6
lav92 <xxkukucxx@gmail.com>
2023-11-26 01:33:37 +0300

    create sidebar: popular news, tag clouds, list of categories

--- footbal/news/templatetags/news_tags.py
+++ footbal/news/templatetags/news_tags.py
@@ -1,10 +1,28 @@
 from django import template
-from news.models import Category
+from news.models import Category, News, Tag
 
 register = template.Library()
 
 
-@register.inclusion_tag('news/category_list.html')
+@register.inclusion_tag('news/include/category_list.html')
 def show_categories():
+    cats = Category.objects.all()
+    return {'cats': cats}
+
+
+@register.inclusion_tag('news/include/popular_news.html')
+def popular_news():
+    news = News.objects.all().order_by('-views')[:5]
+    return {'news': news}
+
+
+@register.inclusion_tag('news/include/tag_clouds.html')
+def tag_clouds():
+    tags = Tag.objects.all()
+    return {'tags': tags}
+
+
+@register.inclusion_tag('news/include/sidebar-categories.html')
+def sidebar_categories():
     cats = Category.objects.all()
     return {'cats': cats}
