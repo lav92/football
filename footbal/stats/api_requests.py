079322dadef2e07a6fe0e7e57ceeef80da5a20f3
lav92 <xxkukucxx@gmail.com>
2023-12-04 23:30:18 +0300

    добавление нового приложения stats с единственным шаблоном на котором отображается актуальная таблица АПЛ и список бомбардиров. Данные берутся через API сайта https://www.football-data.org, создание собственных команд django-admin для добавления и обновления данных

--- footbal/stats/api_requests.py
+++ footbal/stats/api_requests.py
@@ -6,35 +6,17 @@ headers = {'X-Auth-Token': '58e6cb6d63de4b5fa5e98defa3b17d3d'}
 def get_teams():
     teams_list = list()
     response = requests.get('https://api.football-data.org/v4/competitions/PL/standings', headers=headers)
-    for k, v in response.json().items():
-        if k == 'standings':
-            for key, value in v[0].items():
-                if key == 'table':
-                    for pos in value:
-                        for row, column in pos.items():
-                            if row == 'team':
-                                teams_list.append(column)
+    for item in response.json().get('standings')[0].get('table'):
+        teams_list.append(item.get('team'))
 
     return teams_list
 
 
 def get_table():
-    result = list()
     response = requests.get('https://api.football-data.org/v4/competitions/PL/standings', headers=headers)
-    for k, v in response.json().items():
-        if k == 'standings':
-            for key, value in v[0].items():
-                if key == 'table':
-                    for pos in value:
-                        result.append(pos)
-    return result
+    return response.json().get('standings')[0].get('table')
 
 
 def get_scorers():
-    result = list()
     response = requests.get('https://api.football-data.org/v4/competitions/PL/scorers', headers=headers)
-    for key, value in response.json().items():
-        if key == 'scorers':
-            for item in value:
-                result.append(item)
-    return result
+    return response.json().get('scorers')
